8418cf0fbfa42713ab42e72c20abccdc02e5171f
Rahul Singh <docoderahul@gmail.com>
2025-08-12 22:58:12 +0530

    refactor: improve message fetching and error handling in dashboard page

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import UserModel, { Message } from "@/model/User";
+import { Message } from "@/model/User";
 import { AcceptMessageSchema } from "@/schemas/AcceptMessageSchema";
+import { ApiResponse } from "@/types/ApiResponce";
 import { zodResolver } from "@hookform/resolvers/zod";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useSession } from "next-auth/react";
-import { useCallback, useState } from "react";
+// import { useSession } from "next-auth/react";
+import { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 function page() {
   const [messages, setMessage] = useState<Message[]>([]);
@@ -17,7 +20,7 @@ function page() {
     setMessage(messages.filter((message) => message._id !== messageID));
   };
 
-  // const { date: session } = useSession();
+  const { data: session } = useSession();
   const form = useForm({
     resolver: zodResolver(AcceptMessageSchema),
   });
@@ -31,9 +34,53 @@ function page() {
       const res = await axios.get("/api/accept-messages");
       setValue("acceptMessage", res.data.isAcceptingMessage);
     } catch (error) {
-      console.error("Some Error founding in fetching the data", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+      // console.error("Some Error founding in fetching the data", error);
+      toast.error("Error", {
+        description:
+          axiosError.response?.data.message ||
+          "failed to fetch message setting",
+      });
+    } finally {
+      setSwitching(false);
     }
   }, [setValue]);
+
+  const fetchMessages = useCallback(async (refresh: boolean = false) => {
+    setLoading(true);
+    setSwitching(false);
+    try {
+      const res = await axios.get<ApiResponse>("/api/get-messages");
+      setMessage(res.data.messages || []);
+      toast.info("Refresh Messsages", {
+        description: "Showing latest messages",
+      });
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      // console.error("Some Error founding in fetching the data", error);
+      toast.error("Error", {
+        description:
+          axiosError.response?.data.message ||
+          "failed to fetch message setting",
+      });
+    } finally {
+      setSwitching(false);
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!session || !session.user) return fetchAcceptMessage();
+    fetchMessages();
+  }, [setValue, session, fetchAcceptMessage, fetchMessages]);
+
+  //handle switch chnages
+  const handleSwitchChange = async () => {
+    try {
+      axios.post("/api/accept-messages", { acceptMessages: !acceptMessages });
+    } catch (error) {}
+  };
+
   return <div>Dashboard</div>;
 }
 
